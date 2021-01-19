2f894c49be7d78863d1bf0337a0643c70f17468f
Jorge Ramírez <72385150+JorgeRamirezDEV@users.noreply.github.com>
2021-01-19 20:00:06 +0100

    .

--- Script.js
+++ Script.js
@@ -36,3 +36,69 @@ $(function () {
       }
   });
 });
+
+
+// Validar formulario
+
+$("input#nombre").keydown(function(){
+
+  if(/^[a-zA-Z]*$/.test($(this).val()) && $(this).val() !== ""){
+      console.log("funciona");
+      $(this).css("border", "2px solid green");
+      $("input#apellido").prop("disabled", false);
+  }else{
+      console.log("error");
+      $(this).css("border", "2px solid red");
+      $("input#apellido").prop("disabled", true);
+  }
+
+})
+
+$("input#apellido").keydown(function(){
+
+  if(/^[a-zA-Z]*$/.test($(this).val()) && $(this).val() !== ""){
+      $(this).css("border", "2px solid green");
+      $("input#email").prop("disabled", false);
+  }else{
+      $(this).css("border", "2px solid red");
+      $("input#email").prop("disabled", true);
+  }
+
+})
+
+
+$("input#email").keydown(function(){
+
+  if(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test($(this).val()) && $(this).val() !== ""){
+      $(this).css("border", "2px solid green");
+      $("input#pais").prop("disabled", false);
+  }else{
+      $(this).css("border", "2px solid red");
+      $("input#pais").prop("disabled", true);
+  }
+
+})
+
+$("input#pais").keydown(function(){
+
+  if(/^[a-zA-Z]*$/.test($(this).val()) && $(this).val() !== ""){
+      $(this).css("border", "2px solid green");
+      $("input#edad").prop("disabled", false);
+  }else{
+      $(this).css("border", "2px solid red");
+      $("input#edad").prop("disabled", true);
+  }
+
+})
+
+$("input#edad").keydown(function(){
+
+  if(/^(\d{1,2})-(\d{1,2})-(\d{4})$/.test($(this).val()) && $(this).val() !== ""){
+      $(this).css("border", "2px solid green");
+      $("input#enviar").prop("disabled", false);
+  }else{
+      $(this).css("border", "2px solid red");
+      $("input#enviar").prop("disabled", true);
+  }
+
+})
