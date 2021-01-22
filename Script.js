3ba206531f417bc06f77b677d90e89a37bbd0a60
Jorge Ramírez <72385150+JorgeRamirezDEV@users.noreply.github.com>
2021-01-22 17:43:35 +0100

    .

--- Script.js
+++ Script.js
@@ -39,106 +39,111 @@ $(document).ready(function(){
 
 $("input#nombre").keydown(function(){
 
-  if(/^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$/.test($(this).val()) && $(this).val() !== ""){
-      console.log("funciona");
-      $(this).css("border", "2px solid green");
-      $("input#apellido").prop("disabled", false);
-  }else{
-      console.log("error");
-      $(this).css("border", "2px solid red");
-      $("input#apellido").prop("disabled", true);
-  }
+    if(/^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$/.test($(this).val()) && $(this).val() !== ""){
+        console.log("funciona");
+        $(this).css("border", "2px solid green");
+        $("input#apellido").prop("disabled", false);
+    }else{
+        console.log("error");
+        $(this).css("border", "2px solid red");
+        $("input#apellido").prop("disabled", true);
+    }
 
 })
 
 $("input#apellido").keydown(function(){
 
-  if(/^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$/.test($(this).val()) && $(this).val() !== ""){
-      $(this).css("border", "2px solid green");
-      $("input#email").prop("disabled", false);
-  }else{
-      $(this).css("border", "2px solid red");
-      $("input#email").prop("disabled", true);
-  }
+    if(/^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$/.test($(this).val()) && $(this).val() !== ""){
+        $(this).css("border", "2px solid green");
+        $("input#email").prop("disabled", false);
+    }else{
+        $(this).css("border", "2px solid red");
+        $("input#email").prop("disabled", true);
+    }
 
 })
 
 
 $("input#email").keydown(function(){
 
-  if(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test($(this).val()) && $(this).val() !== ""){
-      $(this).css("border", "2px solid green");
-      $("input#pais").prop("disabled", false);
-  }else{
-      $(this).css("border", "2px solid red");
-      $("input#pais").prop("disabled", true);
-  }
+    if(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test($(this).val()) && $(this).val() !== ""){
+        $(this).css("border", "2px solid green");
+        $("input#pais").prop("disabled", false);
+    }else{
+        $(this).css("border", "2px solid red");
+        $("input#pais").prop("disabled", true);
+    }
 
 })
 
 $("input#pais").keydown(function(){
 
-  if(/^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$/.test($(this).val()) && $(this).val() !== ""){
-      $(this).css("border", "2px solid green");
-      $("input#edad").prop("disabled", false);
-  }else{
-      $(this).css("border", "2px solid red");
-      $("input#edad").prop("disabled", true);
-  }
+    if(/^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$/.test($(this).val()) && $(this).val() !== ""){
+        $(this).css("border", "2px solid green");
+        $("input#edad").prop("disabled", false);
+    }else{
+        $(this).css("border", "2px solid red");
+        $("input#edad").prop("disabled", true);
+    }
 
 })
 
 $("input#edad").keydown(function(){
 
-  if(/^(\d{1,2})-(\d{1,2})-(\d{4})$/.test($(this).val()) && $(this).val() !== ""){
-      $(this).css("border", "2px solid green");
-      $("input#enviar").prop("disabled", false);
-  }else{
-      $(this).css("border", "2px solid red");
-      $("input#enviar").prop("disabled", true);
-  }
+    if(/^(\d{1,2})-(\d{1,2})-(\d{4})$/.test($(this).val()) && $(this).val() !== ""){
+        $(this).css("border", "2px solid green");
+        $("input#enviar").prop("disabled", false);
+    }else{
+        $(this).css("border", "2px solid red");
+        $("input#enviar").prop("disabled", true);
+    }
 
 })
 
 
+
 // Mostrar testimonios
 
-function recogerTestimonios(){
-  $.ajax({
-      dataType: "json",
-      url: "testimonios.json",
-      data: "data",
-      success: function(response){
-          for (let i=0; i<3; i++){
-              let random = Math.floor(Math.random() * 7);
-              $("label#nombre"+i.toString()).text(response.testimonios[random].nombre);
-              $("p#testimonio"+i.toString()).text(response.testimonios[random].texto);
-              $("p#fecha"+i.toString()).text(response.testimonios[random].fecha);
-          }
-      }
-  })
-  setTimeout(recogerTestimonios, 10000);
+function mostrarTestimonios(){
+    $.ajax({
+        dataType: "json",
+        url: "testimonios.json",
+        data: "data",
+        success: function(response){
+            for (let i=0; i<3; i++){
+                let random = Math.floor(Math.random() * 7);
+                $("label#nombre"+i.toString()).text(response.testimonios[random].nombre);
+                $("p#testimonio"+i.toString()).text(response.testimonios[random].texto);
+                $("p#fecha"+i.toString()).text(response.testimonios[random].fecha);
+            }
+        }
+    })
+  setTimeout(mostrarTestimonios, 10000);
 }
 
-recogerTestimonios(); 
+mostrarTestimonios(); 
 
 
 
 // Mostrar Productos
 
 function mostrarProductos(){
-  $.ajax({
-      dataType: "json",
-      url: "productos.json",
-      data: "data",
-      success: function(response){
-          for (let i=0; i<3; i++){
-              $("div#producto"+i.toString()).prepend('<img id="fotosproductos" src="'+response.productos[i].imagen+'" alt="'+response.productos[i].nombre+'"/>');
-              $("div#producto"+i.toString()).prepend('<label id="nombreProductos">'+response.productos[i].nombre+'</label>');
-              $("div#producto"+i.toString()).prepend('<label id="precioProductos">'+response.productos[i].precio+'</label>');
-          }
-      }
-  })
+    $.ajax({
+        dataType: "json",
+        url: "productos.json",
+        data: "data",
+        success: function(response){
+            for (let i=0; i<3; i++){
+                $("div#producto"+i.toString()).prepend('<img id="fotosproductos" src="'+response.productos[i].imagen+'" alt="'+response.productos[i].nombre+'"/>');
+                $("div#producto"+i.toString()).prepend('<label id="nombreProductos">'+response.productos[i].nombre+'</label>');
+                $("div#producto"+i.toString()).prepend('<label id="precioProductos">'+response.productos[i].precio+'</label>');
+            }
+        },
+        error: function(){
+            console.log("Error al obtener los productos");
+            setTimeout(recogerProductos, 5000);
+        }
+    })
 }
 
 mostrarProductos();
@@ -170,16 +175,23 @@ document.addEventListener('scroll', function (e) {
 });
 
 
-// Localización, la ciudad a veces falla
+// Localización, la ciudad a veces falla, al menos conmigo no es precisa
 
+var requestUrl = "http://ip-api.com/json";
 
-
+$.ajax({
+  url: requestUrl,
+  type: 'GET',
+  success: function(json)
+  {
+    console.log("Ciudad: " + json.city);
+    console.log("Región: " +  json.regionName);
+    console.log( "País: " + json.country);
+  }
+});
 
 
 // // Cambiar vista de testimonios
-
-
-
 
 
 var lista = false;
@@ -192,4 +204,4 @@ $( "#cambiarVista" ).click(function() {
    else{ 
     $(".testimonio").css("display","grid");
    }
- });
+});
